a17e777a1f66db0ecdf25dac6359429021b27a48
Fabian Duran <fabiandurandev@gmail.com>
2025-06-16 17:58:28 -0400

    Agregando capeta Modal mas un modal para cliente no registrado

---
+++ src/components/Modals/ModalClient.tsx
@@ -0,0 +1,50 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+type Props = {};
+
+function ModalClient() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent borderRadius={"10"}>
+          <ModalHeader borderTopRadius={"10"} bg={"blue.600"}>
+            Aviso
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sit
+              quod tempora. Deserunt nemo porro, consequatur perferendis odio
+              blanditiis hic corporis, laboriosam id rem tempora harum,
+              similique nostrum unde. Nobis.
+            </Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default ModalClient;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import ClientSearch from "../../components/ClientSearch";
+import ModalClient from "../../components/Modals/ModalClient";
 import ProductSelector from "../../components/ProductSelector";
 function Home() {
   return (
     <>
       <ClientSearch />
       <ProductSelector />
+      <ModalClient />
     </>
   );
 }
