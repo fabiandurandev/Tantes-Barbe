0ad70870958a8f8e261314c02c3f1fe10d90c30e
fabiandurandev <fabiandurandev@gmail.com>
2025-07-02 23:50:23 -0400

    Revert "Funcionalidades del módulo (Agregar Producto)"

--- src/components/Modals/AddProduct.tsx
+++ src/components/Modals/AddProduct.tsx
@@ -12,60 +12,22 @@ import {
   FormControl,
   FormLabel,
   Input,
-  Heading,
-  Text,
+  Heading
 } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
-import {
-  addProductSchema,
-  type addProductFormType,
-} from "../../schemas/AddProduct";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { UseAddProduct } from "../../hooks/UseAddProduct";
-import CodigoProducto from "./CodigoProducto";
 
-type Props = {
-  addProductModal: {
-    isOpen: boolean;
-    onClose: () => void;
-  };
-};
-
-function AddProduct({ addProductModal }: Props) {
-  const {
-    register,
-    reset,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<addProductFormType>({
-    resolver: zodResolver(addProductSchema),
-  });
-
-  const { mutate, data: dataAddProduct, error } = UseAddProduct();
-
-  const onSubmit = (data: addProductFormType) => {
-    const productLoad = {
-      nombreProducto: data.nombreProducto,
-      codigoProducto: data.codigoProducto,
-      stock: 0,
-      precioProducto: data.precioProducto,
-    };
-    mutate(productLoad, {
-      onSuccess: () => {
-        reset();
-        console.log(dataAddProduct);
-      },
-    });
-  };
+function AddProduct() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
-      <Modal isOpen={addProductModal.isOpen} onClose={addProductModal.onClose}>
+      <Button onClick={onOpen}>Agregar Producto</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent borderRadius="20px">
-          <ModalHeader
-            borderTopRadius="20px"
-            bg="blue.600"
+          <ModalHeader 
+            borderTopRadius="20px" 
+            bg="blue.600" 
             color="white"
             fontSize="lg"
             fontWeight="bold"
@@ -73,68 +35,68 @@ function AddProduct({ addProductModal }: Props) {
             Agregar nuevos productos
           </ModalHeader>
           <ModalCloseButton color="white" />
+          
+          <ModalBody py={4}>
+            <Stack spacing={4}>
+              <Heading as="h3" size="md" fontWeight="semibold">Datos del Producto</Heading>
+              
+              <FormControl>
+                <FormLabel>Código:</FormLabel>
+                <Input 
+                  placeholder="" 
+                  borderWidth={2}
+                  borderRadius="md"
+                />
+              </FormControl>
 
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <ModalBody py={4}>
-              <Stack spacing={4}>
-                <Heading as="h3" size="md" fontWeight="semibold">
-                  Datos del Producto
-                </Heading>
-                <FormControl>
-                  <FormLabel>Código:</FormLabel>
-                  <Input
-                    {...register("codigoProducto")}
-                    placeholder=""
-                    borderWidth={2}
-                    borderRadius="md"
-                  />
-                  {error && <Text color={"red"}>El codigo ya existe</Text>}
-                  {errors && (
-                    <Text color={"red"}>{errors.codigoProducto?.message}</Text>
-                  )}
-                  <FormLabel>Descripción:</FormLabel>
-                  <Input
-                    {...register("nombreProducto")}
-                    placeholder=""
-                    borderWidth={2}
-                    borderRadius="md"
-                  />
-                  {errors && (
-                    <Text color={"red"}>{errors.nombreProducto?.message}</Text>
-                  )}
-                  <FormLabel>Precio:</FormLabel>
-                  <Input
-                    {...register("precioProducto")}
-                    type="number"
-                    placeholder=""
-                    borderWidth={2}
-                    borderRadius="md"
-                  />
-                  {errors && (
-                    <Text color={"red"}>{errors.precioProducto?.message}</Text>
-                  )}
-                </FormControl>
-              </Stack>
-            </ModalBody>
-            <ModalFooter>
-              <Button
-                type="submit"
-                colorScheme="blue"
-                mr={3}
-                borderRadius="md"
-                leftIcon={<span>+</span>}
-              >
-                Agregar
-              </Button>
-              <Button
-                variant="outline"
-                onClick={addProductModal.onClose}
-                borderRadius="md"
-              >
-                CANCELAR
-              </Button>
-            </ModalFooter>
-          </form>
+              <FormControl>
+                <FormLabel>Descripción:</FormLabel>
+                <Input 
+                  placeholder="" 
+                  borderWidth={2}
+                  borderRadius="md"
+                />
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Precio:</FormLabel>
+                <Input 
+                  type="number" 
+                  placeholder="" 
+                  borderWidth={2}
+                  borderRadius="md"
+                />
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Cantidad:</FormLabel>
+                <Input 
+                  type="number" 
+                  placeholder="" 
+                  borderWidth={2}
+                  borderRadius="md"
+                />
+              </FormControl>
+            </Stack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button 
+              colorScheme="blue" 
+              mr={3}
+              borderRadius="md"
+              leftIcon={<span>+</span>}
+            >
+              Agregar
+            </Button>
+            <Button 
+              variant="outline" 
+              onClick={onClose}
+              borderRadius="md"
+            >
+              CANCELAR
+            </Button>
+          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
--- src/pages/Productos/index.tsx
+++ src/pages/Productos/index.tsx
@@ -1,96 +1,101 @@
-import {
-  Box,
-  HStack,
-  Button,
-  useColorModeValue,
-  Flex,
-  Text,
-  VStack,
-  Image,
-  useDisclosure,
-} from "@chakra-ui/react";
-import { useEffect } from "react";
-import AddProduct from "../../components/Modals/AddProduct";
-import UpdateProduct from "../../components/Modals/UpdateProduct";
-import ListProduct from "../../components/Modals/ListProduct";
-import CodigoProducto from "../../components/Modals/CodigoProducto";
+import { Box, HStack, Button, useColorModeValue, Flex, Text, VStack, Image } from "@chakra-ui/react";
 
-const ProductMenu = () => {
+const ServiceMenu = () => {
   const buttonBg = useColorModeValue("blue.500", "blue.200");
   const buttonColor = useColorModeValue("white", "gray.800");
   const buttonHoverBg = useColorModeValue("blue.600", "blue.300");
 
-  const addProductModal = useDisclosure();
-
   return (
-    <Flex minH="75vh" direction="column" justify="center" align="center" p={6}>
-      <Box p={6} maxW="4xl" mx="auto" textAlign="center">
-        <HStack spacing={20} justify="center">
-          <VStack>
-            <Button
-              onClick={addProductModal.onOpen}
-              size="lg"
-              bg={buttonBg}
-              color={buttonColor}
-              _hover={{ bg: buttonHoverBg }}
-              minW="300px"
-              minH="300px"
-              leftIcon={
-                <Image src="/img/agregar.png" boxSize="180px" alt="Agregar" />
-              }
-            ></Button>
-            <Text fontWeight="bold" fontSize="lg">
-              AGREGAR NUEVO PRODUCTO
-            </Text>
-            <AddProduct addProductModal={addProductModal} />
-          </VStack>
-          Icon={<Image src="/img/linea.png" boxSize="180px" alt="Agregar" />}
-          <VStack>
-            <Button
-              size="lg"
-              bg={buttonBg}
-              color={buttonColor}
-              _hover={{ bg: buttonHoverBg }}
-              minW="300px"
-              minH="300px"
-              leftIcon={
-                <Image
-                  src="/img/modificar.png"
-                  boxSize="180px"
-                  alt="Modificar"
-                />
-              }
-            ></Button>
-            <Text fontWeight="bold" fontSize="lg">
-              MODIFICAR PRODUCTO
-            </Text>
-            <UpdateProduct />
-          </VStack>
-          Icon={<Image src="/img/linea.png" boxSize="180px" alt="Agregar" />}
-          <VStack>
-            <Button
-              size="lg"
-              bg={buttonBg}
-              color={buttonColor}
-              _hover={{ bg: buttonHoverBg }}
-              minW="300px"
-              minH="300px"
-              leftIcon={
-                <Image
-                  src="/img/consultar.png"
-                  boxSize="180px"
-                  alt="Consultar"
-                />
-              }
-            ></Button>
-            <Text fontWeight="bold" fontSize="lg">
-              CONSULTAR PRODUCTOS
-            </Text>
-          </VStack>
-        </HStack>
-      </Box>
+     <Flex
+      minH="75vh"          
+      direction="column"    
+      justify="center"      
+      align="center"        
+      p={6}
+    >
+    <Box p={6} maxW="4xl" mx="auto" textAlign="center">
+      
+      
+      <HStack spacing={20} justify="center">
+        <VStack>
+        <Button
+          size="lg"
+          bg={buttonBg}
+          color={buttonColor}
+          _hover={{ bg: buttonHoverBg }}
+          minW="300px"
+          minH="300px"
+          leftIcon={
+            <Image 
+            src="/img/agregar.png" 
+            boxSize="180px"
+            alt="Agregar"
+            />
+            }
+        >
+        </Button>
+        <Text fontWeight="bold" fontSize="lg">AGREGAR NUEVO PRODUCTO</Text>
+        </VStack>
+
+        Icon={
+            <Image 
+            src="/img/linea.png" 
+            boxSize="180px"
+            alt="Agregar"
+            />
+            }
+        
+        <VStack>
+        <Button
+          size="lg"
+          bg={buttonBg}
+          color={buttonColor}
+          _hover={{ bg: buttonHoverBg }}
+          minW="300px"
+          minH="300px"
+          leftIcon={
+            <Image 
+            src="/img/modificar.png" 
+            boxSize="180px"
+            alt="Modificar"
+            />
+            }
+        >
+        </Button>
+        <Text fontWeight="bold" fontSize="lg">MODIFICAR PRODUCTO</Text>
+        </VStack>
+        
+        Icon={
+            <Image 
+            src="/img/linea.png" 
+            boxSize="180px"
+            alt="Agregar"
+            />
+            }
+
+        <VStack>
+        <Button
+          size="lg"
+          bg={buttonBg}
+          color={buttonColor}
+          _hover={{ bg: buttonHoverBg }}
+          minW="300px"
+          minH="300px"
+          leftIcon={
+            <Image 
+            src="/img/consultar.png" 
+            boxSize="180px"
+            alt="Consultar"
+            />
+            }
+        >
+        </Button>
+        <Text fontWeight="bold" fontSize="lg">CONSULTAR PRODUCTOS</Text>
+        </VStack>
+      </HStack>
+    </Box>
     </Flex>
   );
 };
 
-export default ProductMenu;
+export default ServiceMenu;
